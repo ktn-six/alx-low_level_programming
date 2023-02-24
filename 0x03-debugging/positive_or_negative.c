a5b5cd18a56b69085e1ebc57cc766b6cbc7932ce
ktn-six <tk6.ngeno@gmail.com>
2023-02-24 06:14:29 -0800

    debbuging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - check for +ve or -ve numbers
+ *
+ * @i: the number to be checked
+ * Return always o
+ */
+void positive_or_negative(int i)
+ {
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else 
+		 printf("%d is positive\n", i);
+ }
